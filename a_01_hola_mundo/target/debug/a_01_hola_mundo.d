5df9e38cbd26cf478d7fb0c10fa957d2090af42d
SrLLamaDev <srllamadev@gmail.com>
2025-08-21 07:31:28 -0400

    001 hello wolrd in rust

---
+++ a_01_hola_mundo/target/debug/a_01_hola_mundo.d
@@ -0,0 +1,1 @@
+C:\Users\yamil\Desktop\RUST-LEARN\RUST-LEARN\a_01_hola_mundo\target\debug\a_01_hola_mundo.exe: C:\Users\yamil\Desktop\RUST-LEARN\RUST-LEARN\a_01_hola_mundo\src\main.rs
---
+++ a_01_hola_mundo/target/release/deps/a_01_hola_mundo.d
@@ -0,0 +1,5 @@
+C:\Users\yamil\Desktop\RUST-LEARN\RUST-LEARN\a_01_hola_mundo\target\release\deps\a_01_hola_mundo.d: src\main.rs
+
+C:\Users\yamil\Desktop\RUST-LEARN\RUST-LEARN\a_01_hola_mundo\target\release\deps\a_01_hola_mundo.exe: src\main.rs
+
+src\main.rs:
